7d22e696ae2903613f9ca69117d17348707b4ff9
mobo07 <62022548+mobo07@users.noreply.github.com>
2022-07-24 21:12:34 +0100

    Update passport-google.js

--- config/passport/passport-google.js
+++ config/passport/passport-google.js
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/profile",
+      callbackURL: " https://mysterious-springs-77605.herokuapp.com/auth/google/profile",
     },
     (accessToken, refreshToken, profile, cb) => {
       User.findOne({ googleId: profile.id }).then((user) => {
